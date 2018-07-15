bfbb4e0255e4c1a8382dad64f7205ff3fdd34e0e
lubamark <lubamark@mail.ru>
2018-07-16 00:02:55 +0700

    add pug

--- source/index.js
+++ source/index.js
@@ -1,3 +1,3 @@
 import createMenu from './menu';
-const menu = createMenu(['Главная','Обо мне','Портфолио'], 'menu');
+const menu = createMenu(['Главная','Обо мне','Портфоли0'], 'menu');
 document.body.appendChild(menu);
